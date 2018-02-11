c8666c0b87d716a98c174d584d5fe1f748ae82ad
Bryan Martin <bmart847@gmail.com>
2018-02-11 03:05:19 -0500

    add node to table

--- sqlconnection.py
+++ sqlconnection.py
@@ -6,31 +6,68 @@ Created on Fri Feb  2 13:02:46 2018
 """
 
 from sqlalchemy import *
+from node import Node
 
 class SQLController:
 
+    # Lets use getters and setter, feels safer....
     _sql_engine = None
     _sql_path = None
     
+    _metadata = None
+    
+    node_table = None
+    host_table = None
+    
+    # Initial creation of the controller. One for an instance.
     def __init__(self, path):
         self._sql_path = path
         self._sql_engine = create_engine(self._sql_path)
+        self._metadata = MetaData()
+        self.node_table = self.get_node_table()
      
+    # Simple re-creation of engine
     def reset(self):
         self._sql_engine = create_engine(self._sql_path)
         
+    # Getters and Setters
     def get_engine(self):
         return _sql_engine;
     
     
+    # Member Functions
+    
+    """ Executes a generic query and returns the result """
+    def execute(self, query):
+        result = self._sql_engine.execute(query)
+        return result
+    
+    def add_node(self, node):
+        if type(node) is Node:
+            node_ip = node.get_ip()
+            node_lastConn = node.get_last_connection()
+            node_inUse = node.get_inUse()
+            node_disabled = node.get_disabled()
+            to_insert = self.node_table.insert().values(ipAddr = node_ip, ipinUse = node_inUse, disabled = node_disabled, lastConnect = node_lastConn)
+            result = self.execute(to_insert)
+            return result
+        else:
+            print("Not a node")
+        
+    
+
+    
     """ The SQL Controller contains the structures of all tables
     involved. Re-run any table creation functions after making changes to this
-    section """
+    section.
+    Add any other tables in a similar manner.
+    """
     
-    def get_node_table(self, metadata):
+    def get_node_table(self):
         # Provides a python object of our Node table
-        node_table = Table('Nodes', metadata,
+        node_table = Table('Nodes', self._metadata,
             Column('id', Integer, primary_key=True),
+            Column('ipAddr', String),
             Column('inUse', Boolean),
             Column('disabled', Boolean),
             Column('lastConnect', DATETIME)
@@ -61,5 +98,5 @@ class SQLController:
     
     def get_report_table(self, metadata):
         # Provides a python object of our User Table
-        user_table = Table('Users', metadata)
+        user_table = Table('Reports', metadata)
         return user_table
