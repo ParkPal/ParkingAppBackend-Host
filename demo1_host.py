d90ad865052c7481e565319f74717397b6620bdb
bmart847 <bmart847@gmail.com>
2018-02-13 05:19:57 +0000

    Changes made running on the raspberry pi, some just formatting

--- demo1_host.py
+++ demo1_host.py
@@ -12,22 +12,24 @@ from node import Node
 def main():
     """ Create an object that represents this lot. """
     this_lot = Host("new_lot")
-    
+
     """ Creates a way of interacting with the local sql database """
-    sql_controller = SQLController("mysql://manager:password@localhost:3306/host_db")
-    
+    sql_controller = SQLController("mysql://manager:password@localhost/host_db")
+
     """ Creates a way of listening for messages from the node """
-    mesh_connection = MeshConnection("127.0.0.1", 12345)
-    
+    mesh_connection = MeshConnection("192.168.2.1", 12345)
+    mesh_connection.init_conn()
+
     """ Creates a way of broadcasting messages to the server """
-    server__connection = WebConnection("SERVER_IP", SERVER_PORT)
-    
+    #server__connection = WebConnection("SERVER_IP", SERVER_PORT)
+
     while True:
         """ Wait for object to be sent from a node """
         obj = mesh_connection.listen()
-        
+
         """ Relay data to the server """
         if type(obj) is Node:
+            sql_controller.add_node(obj)
             print("Node recieved")
         else:
             print("Recieved garbage from node")
